2c7a60764ae05428a023d3e6149926b3f310c15b
Sooraj Boominathan <sooraj.boomi@gmail.com>
2016-09-17 18:50:57 -0400

    fixed handlebars problem

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+/*app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');*/
+var exphbs = require('express-handlebars');
+app.engine('.hbs',exphbs({extname: '.hbs'}));
+
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
