c0dca387cd6537ca0c71e836455465bcf682495f
Sooraj Boominathan <sooraj.boomi@gmail.com>
2016-09-17 19:28:51 -0400

    added communication with python script

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,12 @@ router.get('/', function(req, res, next) {
 
 router.post('/find-video', function(req, res, next) {
   res.render('index', { video_name: 'Obama speech' });
+  var pyshell = new pythonShell('./routes/my_script.py');
+  pyshell.on('message', function (message) {
+  // received a message sent from the Python script (a simple "print" statement)
+	  console.log(message);
+	}); 
 });
+
 
 module.exports = router;
