d4dbf7852b83a865db7ae0fe2c5e4dc1eb685966
Christopher Wang <czw@mit.edu>
2016-09-18 03:51:08 -0400

    working

--- routes/create.js
+++ routes/create.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-$(document).ready(function(){
-	console.log("dsd");
-    $("#word-button").click(function(){
-        $("#words").val($("#word-button").text());
-    });
-});
+
 
 router.get('/', function(req, res, next) {
   res.render('create');
