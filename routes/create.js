b0d65cc6dd2aeaee03ba9df3a586b6e463aab0b0
Christopher Wang <czw@mit.edu>
2016-09-18 05:05:59 -0400

    save

--- routes/create.js
+++ routes/create.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
-
+var pythonShell = require('python-shell');
 
 router.get('/', function(req, res, next) {
   res.render('create');
 });
 
-router.get('/submit-text', function(req, res, next) {
-  res.render('create');
+router.post('/submit-text', function(req, res, next) {
+
+  var input_text = req.body.words;
+
+  var pyshell = new pythonShell('./routes/audio.py');
+
+  var dict_string = JSON.stringify(word_dict)
+
+
+  pyshell.send(input_text);
+  pyshell.send(dict_string);
+
+  pyshell.on('message', function (message) {
+  
+  		console.log(message)
+	
+	}); 
+
+  pyshell.end(function (err) {
+  	// if(err) {throw err;}
+  });
+
 });
 
 module.exports = router;
